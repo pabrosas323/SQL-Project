5ac0055fe1deb4755cabee8f31a57b99ead526ed
Pablo Rosas <prosas1@lsu.edu>
2024-07-31 20:26:29 -0400

    Complete code for top_paying_jobs

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,29 @@
+/*
+Question: What are the top-paying data analyst roles?
+- Identify the top 10 highest-paying jobs that are available remotely.
+- Focus on job postings with specified salaries (avoid nulls).
+*/
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+
+FROM
+    job_postings_fact    
+
+LEFT JOIN company_dim
+    ON company_dim.company_id = job_postings_fact.company_id
+
+ WHERE
+    salary_year_avg IS NOT NULL AND
+    job_location = 'Anywhere' AND
+    job_title_short = 'Data Analyst'
+
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
